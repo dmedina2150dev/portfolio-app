2d7936bac30ab1c557815e91c273d176fc7159f1
Dajan Medina <dmedina2150dev@gmail.com>
2021-09-25 02:53:16 -0300

    respaldo pc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomerComponent } from './homer/homer.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'post/:id',
+    component: PostDetailComponent
+  },
+  {
+    path: '',
+    component: HomerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { HomerComponent } from './homer/homer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CardComponent,
+    HeaderComponent,
+    PostDetailComponent,
+    HomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
